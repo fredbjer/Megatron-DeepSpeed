396ca5460b6f5bb6a0de2ec5299eed1cd27892cc
fredbjer <cfworking@163.com>
2023-04-18 06:41:03 +0800

    update
    Signed-off-by: fredbjer <cfworking@163.com>

--- examples/pretrain_gpt_distributed_with_mp6.sh
+++ examples/pretrain_gpt_distributed_with_mp6.sh
@@ -1,13 +1,19 @@
 #! /bin/bash
 
-# Runs the "76B" parameter model
+# Runs the "145B" parameter model
+# 145B- for 80 layers -> 78 layers 
+
+export NCCL_IB_GID_INDEX=0
+export NCCL_SOCKET_IFNAME=bond0 
+export NCCL_IB_DISABLE=0
+export NCCL_DEBUG=INFO
 
 GPUS_PER_NODE=8
 # Change for multinode config
-MASTER_ADDR=10.119.61.15
+MASTER_ADDR=10.119.61.12
 MASTER_PORT=6000
-NNODES=2
-NODE_RANK=1
+NNODES=6
+NODE_RANK=0
 WORLD_SIZE=$(($GPUS_PER_NODE*$NNODES))
 
 DATA_PATH=/data/gpt_data/my-gpt2_text_document
@@ -19,13 +25,13 @@ DISTRIBUTED_ARGS="--nproc_per_node $GPUS_PER_NODE --nnodes $NNODES --node_rank $
 torchrun $DISTRIBUTED_ARGS \
        pretrain_gpt.py \
        --tensor-model-parallel-size 8 \
-       --pipeline-model-parallel-size 2 \
-       --num-layers 60 \
-       --hidden-size 10240 \
-       --num-attention-heads 32 \
+       --pipeline-model-parallel-size 6 \
+       --num-layers 78 \
+       --hidden-size 12288 \
+       --num-attention-heads 64 \
        --micro-batch-size 4 \
-       --global-batch-size 512 \
-       --seq-length 2048 \
+       --global-batch-size 768 \
+       --seq-length 1024 \
        --max-position-embeddings 1024 \
        --train-iters 500000 \
        --lr-decay-iters 320000 \
@@ -44,7 +50,7 @@ torchrun $DISTRIBUTED_ARGS \
        --clip-grad 1.0 \
        --lr-warmup-fraction .01 \
        --checkpoint-activations \
-       --log-interval 10 \
+       --log-interval 1 \
        --save-interval 10 \
        --eval-interval 10 \
        --eval-iters 10 \
